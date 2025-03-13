d39d836291cd0770cb106ac1729d3891d63ce135
Tejas Agrawal <tejasagrawal36@gmail.com>
2025-03-13 08:27:40 -0700

    express setup complate

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB Connected!!! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection Error", error);
